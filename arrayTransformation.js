b422a6e9a8e63487974917a370e68424a69cc4a0
kamrulhaider <kamrulhaider.ssb@gmail.com>
2023-07-21 15:11:26 +0600

    Problem no: 2634

--- arrayTransformation.js
+++ arrayTransformation.js
@@ -1,17 +1,41 @@
 // Problem no: 2635
-var map = function (arr, fn) {
-  let newArr = [];
-  for (let i = 0; i < arr.length; i++) {
-    const element = fn(arr[i], i);
-    newArr.push(element);
-  }
-  return newArr;
-};
+{
+  var map = function (arr, fn) {
+    let newArr = [];
+    for (let i = 0; i < arr.length; i++) {
+      const element = fn(arr[i], i);
+      newArr.push(element);
+    }
+    return newArr;
+  };
 
-const arr = [1, 2, 3];
-const fn = function plusI(n, i) {
-  return n + i;
-};
+  const arr = [1, 2, 3];
+  const fn = function plusI(n, i) {
+    return n + i;
+  };
 
-const result = map(arr, fn);
-console.log(result);
+  const result = map(arr, fn);
+}
+// console.log(result);
+
+// Problem no: 2634
+{
+  var filter = function (arr, fn) {
+    let newArr = [];
+    for (let i = 0; i < arr.length; i++) {
+      const truthy = fn(arr[i], i);
+      if (Boolean(truthy) === true && arr[i] !== 0) {
+        newArr.push(arr[i]);
+      }
+    }
+    return newArr;
+  };
+
+  const arr = [0, 10, 20, 30];
+  const fn = function greaterThan10(n) {
+    return n > 10;
+  };
+
+  const result = filter(arr, fn);
+  // console.log(result);
+}

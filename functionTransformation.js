7b9327a85ae1aba8a1827e501ca6949f5b51d5a5
kamrulhaider <kamrulhaider.ssb@gmail.com>
2023-07-23 00:57:47 +0600

    Problem no: 2703

--- functionTransformation.js
+++ functionTransformation.js
@@ -7,5 +7,15 @@
   };
 
   const fn = compose([(x) => x + 1, (x) => 2 * x]);
-  console.log(fn(4));
+  //   console.log(fn(4));
+}
+
+// Problem no: 2703
+{
+  var argumentsLength = function (...args) {
+    const x = args.length;
+    return x;
+  };
+  const result = argumentsLength([1, 3, 4]);
+  //    console.log(result)
 }

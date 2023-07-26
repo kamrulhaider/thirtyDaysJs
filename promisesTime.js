34b836680fab1f21956a1c75dff8a5890a948df2
kamrulhaider <kamrulhaider.ssb@gmail.com>
2023-07-26 11:26:18 +0600

    Problem no: 2621

--- promisesTime.js
+++ promisesTime.js
@@ -11,3 +11,13 @@
 
   // addTwoPromises(Promise.resolve(2), Promise.resolve(2)).then(console.log);
 }
+
+// Problem no: 2621
+{
+  async function sleep(millis) {
+    return new Promise((resolve) => setTimeout(resolve, millis));
+  }
+
+  // let t = Date.now();
+  // sleep(100).then(() => console.log(Date.now() - t));
+}

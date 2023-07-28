47631d89de65454bcd6933e784e15656859ccd3c
kamrulhaider <kamrulhaider.ssb@gmail.com>
2023-07-28 21:38:37 +0600

    problem no: 2715

--- promisesTime.js
+++ promisesTime.js
@@ -21,3 +21,51 @@
   // let t = Date.now();
   // sleep(100).then(() => console.log(Date.now() - t));
 }
+
+// problem no: 2715
+{
+  var cancellable = function (fn, args, t) {
+    let executed = false;
+
+    const executeFunction = () => {
+      if (!executed) {
+        fn(...args);
+        executed = true;
+      }
+    };
+
+    const cancel = () => {
+      executed = true;
+    };
+
+    setTimeout(executeFunction, t);
+
+    return cancel;
+  };
+
+  // const result = [];
+
+  // const fn = (x) => x * 5;
+  // const args = [2];
+  // const t = 20;
+  // const cancelT = 50;
+
+  // const start = performance.now();
+
+  // const log = (...argsArr) => {
+  //   const diff = Math.floor(performance.now() - start);
+  //   result.push({ "time": diff, "returned": fn(...argsArr) });
+  // };
+
+  // const cancel = cancellable(log, args, t);
+
+  // const maxT = Math.max(t, cancelT);
+
+  // setTimeout(() => {
+  //   cancel();
+  // }, cancelT);
+
+  // setTimeout(() => {
+  //   console.log(result); // [{"time":20,"returned":10}]
+  // }, maxT + 15);
+}
